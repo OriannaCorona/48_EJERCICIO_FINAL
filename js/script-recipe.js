d81c14955c2d1e2754484e0ea2404df117f4357f
OriannaCorona <oriannalievanos12@gmail.com>
2016-12-07 12:32:59 -0600

    agrego etapa 10 ujuuuuu!

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,15 +1,16 @@
-$(document).ready( function(){
+$(document).ready( function() {
 
 	$('.icon-menu').hide();
 	console.log('documento listo');
 
 	quitarClaseMake();
 	agregarClaseMake();
+	volverHome();
 
 });
 
 function quitarClaseMake() {
-	$('.js-show-recipe').on('click', function(){
+	$('.js-show-recipe').on('click', function() {
 		$('.page, .recipe').removeClass('make');
 		$('.js-show-recipe').addClass('active');
 		$('.js-show-make').removeClass('active');			
@@ -17,11 +18,17 @@ function quitarClaseMake() {
 }
 
 function agregarClaseMake() {
-	$('.js-show-make').on('click', function(){
+	$('.js-show-make').on('click', function() {
 		$('.page, .recipe').addClass('make');
 		$('.js-show-make').addClass('active');
 		$('.js-show-recipe').removeClass('active');	
 	});	
+}
+
+function volverHome() {
+	$('.icon-arrow-left-alt').on('click', function() {
+		window.location = 'index.html';
+	});
 }
 	
 
