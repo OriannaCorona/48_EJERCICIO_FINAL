ed3ee97ad187d8541cc79529e4da9ca7dac675d9
OriannaCorona <oriannalievanos12@gmail.com>
2016-12-07 11:53:47 -0600

    agrego la octava etapa completa

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -3,4 +3,21 @@ $(document).ready( function(){
 	$('.icon-menu').hide();
 	console.log('documento listo');
 
+	quitarClaseMake();
+	agregarClaseMake();
+
 });
+
+function quitarClaseMake() {
+	$('.js-show-recipe').on('click', function(){
+		$('.page, .recipe').removeClass('make');			
+	});	
+}
+
+function agregarClaseMake() {
+	$('.js-show-make').on('click', function(){
+		$('.page, .recipe').addClass('make');	
+	});	
+}
+	
+
